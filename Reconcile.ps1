487a74e0aedf474174f905a9e8e1f8197f1d00ab
TVau <tyler.r.vaughan@gmail.com>
2023-02-04 16:57:06 -0800

    Finish update date

--- Reconcile.ps1
+++ Reconcile.ps1
@@ -15,35 +15,45 @@ function Select-File
 }
 function Update-ExcelDates
 {
-    [CmdletBinding(SupportsShouldProcess=$true)]
+    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='High')]
     param(
         [Parameter(Mandatory=$true, ValueFromPipeline)]
-        [Object[]]$Group
+        [Object[]]$Group,
+        [Parameter(Mandatory=$true)]
+        [Object[]]$BudgetXLSX,
+        [Parameter(Mandatory=$true)]
+        [string]$Budgetpath,
+        [Parameter(Mandatory=$true)]
+        [string]$month
     )
     Process
     {
         foreach($singleGroup in $Group)
         {
             $singleGroup.Group | Format-Table Date, Amount, Description | Write-Output
-            if($PSCmdlet.ShouldProcess("Update date in budget", "Are you sure you want to update the date in the budget to $($singleGroup[1].Date)","Update date"))
+            if($PSCmdlet.ShouldProcess("Update date in budget", "Are you sure you want to update the date in the budget to $($singleGroup.Group[1].Date)","Update date"))
             {
-                
+                Try{
+                    $index = $BudgetXLSX.IndexOf($singleGroup.Group[0]) + 3
+                    Export-Excel -Path $Budgetpath -StartRow $index -StartColumn 2 -InputObject $singleGroup.Group[1].Date -WorksheetName $month
+                    $singleGroup.Group[0].Date = $singleGroupGroup.Group[1].Date
+                }
+                Catch{
+                    Write-Error "Close the the file $BudgetPath before continuing."
+                }
             }
         }
     }
 }
 
-
+$budgetPath = "C:\Users\gamers\Documents\Tyler\BudgetScript\One Year Spending Plan - 2023.xlsx"
 $month = Read-Host 'Enter worksheet name in Budget'
-$budgetXLSX = Import-Excel -Path "$home\Documents\Finances\Budget\One Year Spending Plan - 2023.xlsx" -WorksheetName $month -StartRow 2 -StartColumn 2
+$budgetXLSX = Import-Excel -Path $budgetPath -WorksheetName $month -StartRow 2 -StartColumn 2
 
 $amex = Import-Csv -Path (Select-File)
 
 $diff = $budgetXLSX | Where-Object {$_.Owner -eq 'Amex' -and $null -ne $_.Date} | Compare-Object -ReferenceObject $amex -Property {$_.Amount -as [double]}, {$_.Date -as [DateTime]} -PassThru
 
-$diff | Group-Object {$_.Amount -as [Double]} | Where-Object Count -gt 1 | Foreach-Object { 
-    $_.Group | Format-Table Date, Amount, Description | Write-Output
-    if($PSCmdlet.ShouldProcess())
-}
+$diff | Group-Object {$_.Amount -as [Double]} | Where-Object Count -gt 1 | Update-ExcelDates -BudgetXLSX $budgetXLSX -Budgetpath $budgetPath -month $month
 
 $diff | Format-table Date, Description, Amount, 'Card Member', SideIndicator
